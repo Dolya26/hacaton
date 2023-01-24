5ca633eec98d3eedc8b79ae0555e222ee0382982
Dolya26 <doolat.daiyrkulov@gmail.com>
2023-01-24 21:26:42 +0600

    2

--- book_shop/settings.py
+++ book_shop/settings.py
@@ -11,7 +11,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-3s54#hqxf$x#48v&=odaby1l6w*k3^tjx#!b-6zyhx@4we_)5='
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,7 +37,6 @@ INSTALLED_APPS = [
     'ckeditor',
     'account',
     'product',
-    'order',
     'category',
 ]
 
