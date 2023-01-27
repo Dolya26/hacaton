54869c96f26ebc7d42cd79b4264766a4339d7856
Dolya26 <doolat.daiyrkulov@gmail.com>
2023-01-27 21:08:51 +0600

    added rating

--- account/send_mail.py
+++ account/send_mail.py
@@ -2,7 +2,7 @@ from django.core.mail import send_mail
 
 
 def send_confirmation_email(user, code):
-    full_link = f"http://localhost:8003/api/v1/accounts/activate/{code}/"
+    full_link = f"http://localhost:8000/api/v1/accounts/activate/{code}/"
     send_mail(
         'Здравствуйте, активируйте ваш аккаунт!',
         f'Чтобы активировать ваш аккаунт нужно перейти по ссылке: \n{full_link}',
